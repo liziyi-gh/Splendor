725fe9ca641d2b00532e83b52c397ab24938bd32
WIN-GM1I16K9931\Administrator <957576425@qq.com>
2022-02-08 12:27:07 +0800

    aristocracy card update

--- Unity/Assets/Scripts/CardManager.cs
+++ Unity/Assets/Scripts/CardManager.cs
@@ -7,14 +7,15 @@ public class CardManager : MonoBehaviour
 {
     [Header("Œª÷√")]
     [SerializeField] float posY;
+    [SerializeField] int isGz;
 
     void Start()
     {
 
         
-        GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.width);
-        GetComponent<RectTransform>().anchoredPosition = new Vector2(0, posY*Screen.height);
-        GetComponent<GridLayoutGroup>().cellSize = new Vector2(Screen.width *0.18f, Screen.width *0.24f);
+        GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, 0.8f*Screen.width);
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(0, posY*Screen.height+Screen.width*0.49f*isGz);
+        GetComponent<GridLayoutGroup>().cellSize = new Vector2(Screen.width *0.18f, Screen.width *(0.24f-0.06f*isGz));
         GetComponent<GridLayoutGroup>().spacing= new Vector2(Screen.width * 0.015f, Screen.width * 0.015f);
     }
 
