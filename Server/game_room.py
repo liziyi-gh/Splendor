11e9d4c3719687bc1f14b8b9f3794b058ca77bf9
liziyi <gg_liziyi@qq.com>
2022-02-20 17:03:31 +0800

    Server:
    1. fix buy card error

--- Server/game_room.py
+++ Server/game_room.py
@@ -263,10 +263,13 @@ class GameRoom:
                 in_fold = True
             player.addCard(card, operation_info)
 
-            for item in operation_info:
-                gems_type = item["gems_type"]
-                gems_number = item["gems_number"]
-                self.chips[gems_type] += gems_number
+            try:
+                for item in operation_info[1:]:
+                    gems_type = item["gems_type"]
+                    gems_number = item["gems_number"]
+                    self.chips[gems_type] += gems_number
+            except KeyError:
+                print("key error")
 
             if not in_fold:
                 new_card_number = self.card_board.removeCardByNumberThenAddNewCard(
