3edbb1d5ceec89433117f7b6e6363d8e539223ef
liziyi <gg_liziyi@qq.com>
2022-02-21 23:04:14 +0800

    tmp

--- Server/constants.py
+++ Server/constants.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-SERVER_ADDRESS = ('192.168.50.144', 13204)
+SERVER_ADDRESS = ('127.0.0.1', 13204)
 HEADER_LENGTH = 28
 HEADER_FORMAT = "!L3Q"
 CARD_CONFIGUATION_FILE_PATH = "card_configuration.json"
