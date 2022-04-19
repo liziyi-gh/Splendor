339655b6a23833b59087d807a6f177e1b17aec64
liziyi <gg_liziyi@qq.com>
2022-04-19 20:47:17 +0800

    tmp commit

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const title = <h1>璀璨宝石</h1>;
+
   return (
     <div className="App">
       <header className="App-header">
--- web/src/login.jsx
+++ web/src/login.jsx
@@ -1,26 +1,57 @@
 import React from "react";
+import { Router, Route, Link } from "react-router";
+import { GamePage } from "./GamePage.jsx";
 import "./login.css";
 
 export class LoginForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { username: "", password: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const input_type = target.name;
+    this.setState({ [input_type]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(
+      "提交的用户名:" +
+        this.state.username +
+        "\n" +
+        "提交的密码:" +
+        this.state.password
+    );
+    event.preventDefault();
   }
 
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <label>
           用户名:
-          <input type="text" name="name" />
+          <input
+            name="username"
+            type="text"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
           <br />
           密码:
-          <input type="text" name="name" />
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input className="Login-button-blue" type="submit" value="sign-in" />
         </label>
-        <br />
-        <button className="Login-button-blue" type="submit">
-          sign-in
-        </button>
-      </div>
+      </form>
     );
   }
 }
